a236ee0955455489fde2ef11586604f3b03c8515
Adam Chmara <adam.chmara1@gmail.com>
2023-12-29 20:22:26 +0100

    feat: add num validators, input err handling

--- internal/tui/addEntryForm.go
+++ internal/tui/addEntryForm.go
@@ -32,10 +32,12 @@ type (
 const (
 	hotPink  = lipgloss.Color("#FF06B7")
 	darkGray = lipgloss.Color("#767676")
+	darkRed  = lipgloss.Color("#E05252")
 )
 
 var (
 	inputStyle = lipgloss.NewStyle().Foreground(hotPink).MarginLeft(2)
+	errorStyle = lipgloss.NewStyle().Foreground(darkRed).MarginLeft(2)
 )
 
 type model struct {
@@ -92,63 +94,62 @@ func filterSupportedProps(schema notionapi.PropertyConfigs) map[string]notionapi
 	return props
 }
 
-func expValidator(s string) error {
-	// The 3 character should be a slash (/)
-	// The rest should be numbers
-	e := strings.ReplaceAll(s, "/", "")
-	_, err := strconv.ParseInt(e, 10, 64)
-	if err != nil {
-		return fmt.Errorf("EXP is invalid")
+func numberValidator(s string) error {
+	_, err := strconv.ParseInt(s, 10, 64)
+	if err != nil && s != "" {
+		return fmt.Errorf("must be number")
 	}
-
-	// There should be only one slash and it should be in the 2nd index (3rd character)
-	if len(s) >= 3 && (strings.Index(s, "/") != 2 || strings.LastIndex(s, "/") != 2) {
-		return fmt.Errorf("EXP is invalid")
-	}
-
 	return nil
+}
+
+func createTextInput(title string, validator textinput.ValidateFunc) textinput.Model {
+	ti := textinput.New()
+	ti.Placeholder = title
+	ti.Validate = validator
+	return ti
+}
+
+func createContentTextArea() textarea.Model {
+	ta := textarea.New()
+	ta.Placeholder = "Start writing..."
+	ta.SetWidth(50)
+	ta.SetHeight(10)
+	return ta
 }
 
 func initialModel(entryForm EntryInputForm) model {
 	elements := make([]interface{}, len(entryForm.Props)+1)
+
+	validators := map[notionapi.PropertyType]textinput.ValidateFunc{
+		notionapi.PropertyTypeSelect: numberValidator,
+		// TODO: Add validators for other property types
+	}
 
 	titleIdx := 0 // always first
 	idx := 1
 	for title, prop := range entryForm.Props {
 		switch prop {
 		case notionapi.PropertyTypeTitle:
-			ti := textinput.New()
-			ti.Placeholder = title
+			ti := createTextInput(title, nil)
 			ti.Focus()
-
 			elements[titleIdx] = ti
 		case
 			notionapi.PropertyTypeRichText,
-			notionapi.PropertyTypeNumber, // TOOD: number validator
 			notionapi.PropertyTypeSelect,
 			notionapi.PropertyTypeMultiSelect,
 			notionapi.PropertyTypeDate, // TODO: date validator
 			notionapi.PropertyTypeCheckbox,
+			notionapi.PropertyTypeNumber,
 			notionapi.PropertyTypeEmail,
 			notionapi.PropertyTypePhoneNumber: // TODO: phone number validator
-
-			ti := textinput.New()
-			ti.Placeholder = title
-
-			elements[idx] = ti
+			elements[idx] = createTextInput(title, validators[prop])
 			idx++
 		default:
 			fmt.Printf("unsupported property type: %s", prop)
 		}
 	}
 
-	// textarea
-	ta := textarea.New()
-	ta.Placeholder = "Start writing..."
-	ta.SetWidth(50)
-	ta.SetHeight(10)
-
-	elements[len(elements)-1] = ta
+	elements[len(elements)-1] = createContentTextArea()
 
 	// help styles
 	help := help.New()
@@ -231,13 +232,20 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, tea.Batch(cmds...)
 }
 
+func getElemErrMsg(elem textinput.Model) string {
+	if elem.Err != nil {
+		return errorStyle.Render(elem.Err.Error())
+	}
+	return ""
+}
+
 func (m model) View() string {
 	var inputsView strings.Builder
 
 	for _, elem := range m.elements {
 		switch elem := elem.(type) {
 		case textinput.Model:
-			inputsView.WriteString(fmt.Sprintf("%s%s\n", inputStyle.Width(15).Render(elem.Placeholder), elem.View()))
+			inputsView.WriteString(fmt.Sprintf("%s%s%s\n", inputStyle.Width(15).Render(elem.Placeholder), elem.View(), getElemErrMsg(elem)))
 		case textarea.Model:
 			inputsView.WriteString(fmt.Sprintf("\n%s\n%s\n", inputStyle.Width(30).Render("Content"), elem.View()))
 		}
